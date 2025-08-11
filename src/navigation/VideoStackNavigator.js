ea752e5d7c0bebbf85ef883b1d26a72a609121fd
glgibran7 <gibrangl1167@gmail.com>
2025-08-11 20:14:58 +0800

    update video tab

--- src/navigation/VideoStackNavigator.js
+++ src/navigation/VideoStackNavigator.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import VideoScreen from '../screens/VideoScreen';
-import MateriListScreen from '../screens/MateriListScreen';
 
 const Stack = createNativeStackNavigator();
 
 const VideoStackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="ModulList" component={MateriScreen} />
-      <Stack.Screen name="VideoScreen" component={MateriListScreen} />
+      <Stack.Screen name="VideoScreen" component={VideoScreen} />
     </Stack.Navigator>
   );
 };
