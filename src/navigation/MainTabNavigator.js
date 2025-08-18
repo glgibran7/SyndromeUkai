02c5e83b7096e63bc83306e874a90000a521fd5d
glgibran7 <gibrangl1167@gmail.com>
2025-08-18 08:22:24 +0800

    update tab navigator

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,8 +1,8 @@
-// src/navigation/MainTabNavigator.js
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import Ionicons from '@react-native-vector-icons/ionicons';
+import { Platform } from 'react-native';
 
 import HomeScreen from '../screens/HomeScreen';
 import TryOuttackNavigator from './TryOutStackNavigator';
@@ -19,12 +19,18 @@ const MainTabNavigator = () => {
         const currentRouteName =
           navigation?.getState()?.routes[navigation.getState().index]?.name;
 
+        // Menyesuaikan style tab bar
+        const isAndroidWithThreeButtonNav =
+          Platform.OS === 'android' && !Platform.isTV;
+
         return {
           headerShown: false,
-          tabBarStyle:
-            currentRouteName === 'Home'
-              ? { display: 'none' }
-              : { backgroundColor: '#9D2828', height: 60 },
+          tabBarStyle: {
+            backgroundColor: '#9D2828',
+            height: isAndroidWithThreeButtonNav ? 100 : 60, // Untuk perangkat Android dengan navigasi 3 tombol
+            paddingBottom: isAndroidWithThreeButtonNav ? 10 : 0, // Menambah ruang bawah untuk perangkat dengan 3 tombol navigasi
+            display: currentRouteName === 'Home' ? 'none' : 'flex', // Menyembunyikan tab bar ketika berada di Home
+          },
           tabBarLabelStyle: { fontSize: 12 },
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
@@ -58,11 +64,8 @@ const MainTabNavigator = () => {
       }}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-
       <Tab.Screen name="Materi" component={MateriStackNavigator} />
-
       <Tab.Screen name="Video" component={VideoStackNavigator} />
-
       <Tab.Screen
         name="TryOut"
         component={TryOuttackNavigator}
@@ -74,11 +77,14 @@ const MainTabNavigator = () => {
           return {
             tabBarStyle: isExamScreen
               ? { display: 'none' }
-              : { backgroundColor: '#9D2828', height: 60 },
+              : {
+                  backgroundColor: '#9D2828',
+                  height: 100, // Menyesuaikan untuk perangkat dengan 3 tombol
+                  paddingBottom: 10, // Memberikan ruang bawah
+                },
           };
         }}
       />
-
       <Tab.Screen name="Hasil" component={HasilTryOut} />
     </Tab.Navigator>
   );
